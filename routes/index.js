65d03516d7c17e10fea7c6a6cff18e34a80a36dd
Prashantpatill <60505119+Prashantpatill@users.noreply.github.com>
2023-08-02 16:30:35 +1000

    Making final changes for the Glossories Part 2

--- routes/index.js
+++ routes/index.js
@@ -68,14 +68,62 @@ function definifunc()
   {
     id:11,
     term:"Team Capacity",
-    definition:'It is a result when the development team expedites delivering a peice of code which needs to be refactored later on , in other words priorotizing speed delivery over a perfect peice of code  ',
-    reference:'www.productplan.com. (n.d.). What Is Technical Debt? | Definition and Examples. [online] Available at: https://www.productplan.com/glossary/technical-debt/.'
+    definition:'It is the total amount of work a team can sustain over a period of time',
+    reference:'Adeva. (n.d.). Team Capacity Planning In Scrum Teams: [online] Available at: https://adevait.com/blog/agile-work/how-to-do-agile-capacity-planning .'
   },
   {
     id:12,
-    term:"",
+    term:"Behaviour Driven Design",
     definition:'It is a result when the development team expedites delivering a peice of code which needs to be refactored later on , in other words priorotizing speed delivery over a perfect peice of code  ',
     reference:'www.productplan.com. (n.d.). What Is Technical Debt? | Definition and Examples. [online] Available at: https://www.productplan.com/glossary/technical-debt/.'
+  },
+   {
+    id:13,
+    term:"Test Driven Design",
+    definition:'Test-Driven Design is a software development practice where tests for a specific functionality are written before writing the code to implement that functionality. The process begins by writing a failing test, then writing code to make that test pass, and finally refactoring the code to meet quality standards. This process is often repeated in short development cycles',
+    reference:'www.pluralsight.com. (n.d.). TDD vs BDD: What’s the difference? | Pluralsight. [online] Available at: https://www.pluralsight.com/blog/software-development/tdd-vs-bdd.'
+  },
+   {
+    id:14,
+    term:"Behaviour Driven Design",
+    definition:'Behaviour Driven Design is an Agile software development process that encourages collaboration between developers, QA and non-technical or business participants in a software project. It emphasizes the use of a specific language (usually English-like) to describe the behaviour of the system from the users perspective, which helps to clarify the systems behaviour, and the requirements to be implemented. BDD extends TDD by writing test cases in a natural language that non-programmers and domain experts can read',
+    reference:'www.pluralsight.com. (n.d.). TDD vs BDD: What’s the difference? | Pluralsight. [online] Available at: https://www.pluralsight.com/blog/software-development/tdd-vs-bdd.'
+  },
+   {
+    id:15,
+    term:"Paired Programming",
+    definition:'It is a practice where two develpers work on the same peice of code so that they can review them on the go.',
+    reference:'Wikipedia. (2020). Pair programming. [online] Available at: https://en.wikipedia.org/wiki/Pair_programming.'
+  },
+   {
+    id:16,
+    term:"Kanban",
+    definition:'Kanban is a visual management framework which uses a board to represnt the work that is being done thus promoting transperancy and collaboration.',
+    reference:'Radigan, D. (2022). What is kanban? [online] Atlassian. Available at: https://www.atlassian.com/agile/kanban.'
+  },
+   {
+    id:17,
+    term:"Epic",
+    definition:'Epic is a Set of work that can be developed into a story that a scrum team can pick up to develop of fix over a sprint ',
+    reference:'www.knowledgehut.com. (n.d.). Here’s what Subject experts say about an Epic in Agile. [online] Available at: https://www.knowledgehut.com/blog/agile/what-is-an-epic-agile.1'
+  },
+  {
+    id:18,
+    term:"Continuous Delivery",
+    definition:'It is a Software Engineering approach where the application is developed in short cycles enabling the teams tto release a version of the software at regular intervals ',
+    reference:'Amazon Web Services, Inc. (n.d.). What is Continuous Delivery? – Amazon Web Services. [online] Available at: https://aws.amazon.com/devops/continuous-delivery/.'
+  },
+  {
+    id:19,
+    term:"Continuous Deployment",
+    definition:'It is a practice where any change or update on an application that passes the necessary build quaity tests it s released to the customer without the need of an uman approval or intervention',
+    reference:'Atlassian (2019). Continuous integration vs. continuous delivery vs. continuous deployment. [online] Atlassian. Available at: https://www.atlassian.com/continuous-delivery/principles/continuous-integration-vs-delivery-vs-deployment.'
+  },
+  {
+    id:20,
+    term:"Continuous Integration",
+    definition:'It is a software engineering [ractice where the developer working on a feture trie to merge his code to the main branc as often as possible . The changes are validated by creating a build and running a automated tests agains that build',
+    reference:'Atlassian (2019). Continuous integration vs. continuous delivery vs. continuous deployment. [online] Atlassian. Available at: https://www.atlassian.com/continuous-delivery/principles/continuous-integration-vs-delivery-vs-deployment.'
   }
 ];
 }
